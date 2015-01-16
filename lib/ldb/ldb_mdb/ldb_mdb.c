b9dd5fc1bf3211a40c8e6d721f02c6475617e65d
Jakub Hrozek <jakub.hrozek@posteo.se>
2015-01-16 18:52:11 +0100

    ldb_mdb: Add initialization and build code for ldb back end based on mdb
    Allows ldb_connect() to be run with a mdb back end. Includes a stub search
    function because lcb_connect() searches for indexes. Currently the stub only
    returns success and marks the request as done.

---
+++ lib/ldb/ldb_mdb/ldb_mdb.c
@@ -0,0 +1,143 @@
+/*
+   ldb database library using mdb back end
+
+   Copyright (C) Jakub Hrozek 2014
+
+     ** NOTE! The following LGPL license applies to the ldb
+     ** library. This does NOT imply that all of Samba is released
+     ** under the LGPL
+
+   This library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Lesser General Public
+   License as published by the Free Software Foundation; either
+   version 3 of the License, or (at your option) any later version.
+
+   This library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Lesser General Public License for more details.
+
+   You should have received a copy of the GNU Lesser General Public
+   License along with this library; if not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include <lmdb.h>
+#include <errno.h>
+#include <string.h>
+#include <sys/types.h>
+
+#include "ldb_mdb_pvt.h"
+#include "ldb_tev_wrap.h"
+
+#define MDB_URL_PREFIX		"mdb://"
+#define MDB_URL_PREFIX_SIZE	(sizeof(MDB_URL_PREFIX)-1)
+
+static int lmdb_search(struct ldb_tv_module *kv_mod,
+		       struct ldb_request *req,
+		       struct ldb_search *search)
+{
+	return LDB_SUCCESS;
+}
+
+static const struct ldb_tv_ops lmdb_ops = {
+	.search            = lmdb_search,
+};
+
+static int lmdb_pvt_destructor(struct lmdb_private *lmdb)
+{
+	mdb_env_close(lmdb->env);
+	return 0;
+}
+
+static struct lmdb_private *lmdb_pvt_create(TALLOC_CTX *mem_ctx,
+					    struct ldb_context *ldb,
+					    const char *path)
+{
+	struct lmdb_private *lmdb;
+	int ret;
+
+	lmdb = talloc_zero(ldb, struct lmdb_private);
+	if (lmdb == NULL) {
+		return NULL;
+	}
+
+	ret = mdb_env_create(&lmdb->env);
+	if (ret != 0) {
+		ldb_asprintf_errstring(ldb,
+				"Could not create MDB environment %s: %s\n",
+				path, mdb_strerror(ret));
+		talloc_free(lmdb);
+		return NULL;
+	}
+
+	/* Close when lmdb is released */
+	talloc_set_destructor(lmdb, lmdb_pvt_destructor);
+
+	/* MDB_NOSUBDIR implies there is a separate file called path and a
+	 * separate lockfile called path-lock
+	 */
+	ret = mdb_env_open(lmdb->env, path, MDB_NOSUBDIR, 0644);
+	if (ret != 0) {
+		ldb_asprintf_errstring(ldb,
+				"Could not open DB %s: %s\n",
+				path, mdb_strerror(ret));
+		talloc_free(lmdb);
+		return NULL;
+	}
+
+	return lmdb;
+}
+
+static const char *lmdb_get_path(const char *url)
+{
+	const char *path;
+
+	/* parse the url */
+	if (strchr(url, ':')) {
+		if (strncmp(url, MDB_URL_PREFIX, MDB_URL_PREFIX_SIZE) != 0) {
+			return NULL;
+		}
+		path = url + MDB_URL_PREFIX_SIZE;
+	} else {
+		path = url;
+	}
+
+	return path;
+}
+
+static int lmdb_connect(struct ldb_context *ldb, const char *url,
+			unsigned int flags, const char *options[],
+			struct ldb_module **_module)
+{
+	int ret;
+	const char *path;
+	struct ldb_tv_module *kv_mod;
+	struct lmdb_private *lmdb;
+
+	path = lmdb_get_path(url);
+	if (path == NULL) {
+		ldb_debug(ldb, LDB_DEBUG_ERROR, "Invalid mdb URL '%s'", url);
+		return LDB_ERR_OPERATIONS_ERROR;
+	}
+
+	lmdb = lmdb_pvt_create(ldb, ldb, path);
+	if (lmdb == NULL) {
+		return ldb_oom(ldb);
+	}
+
+	ret = ldb_tv_register(lmdb, ldb, "ldb_mdb",
+			      &lmdb_ops, lmdb, &kv_mod);
+	if (ret != LDB_SUCCESS) {
+		talloc_free(lmdb);
+		return ret;
+	}
+
+	*_module = ldb_tv_get_ldb_module(kv_mod);
+	return LDB_SUCCESS;
+}
+
+int ldb_mdb_init(const char *version)
+{
+	LDB_MODULE_CHECK_VERSION(version);
+	return ldb_register_backend("mdb", lmdb_connect, false);
+}
