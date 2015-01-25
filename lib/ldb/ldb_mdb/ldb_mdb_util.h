69b0ff9355f00cfaf6e04a182be70366360cb063
Jakub Hrozek <jakub.hrozek@posteo.se>
2015-01-25 13:07:39 +0100

    ldb_mdb: Add utility functions to convert ldb_dn and ldb_msg to MDB_val

---
+++ lib/ldb/ldb_mdb/ldb_mdb_util.h
@@ -0,0 +1,52 @@
+/*
+   ldb database library using mdb back end
+
+   Copyright (C) Jakub Hrozek 2014
+
+     ** NOTE! The following LGPL license applies to the ldb
+     ** library. This does NOT imply that all of Samba is released
+     ** under the LGPL
+
+   This library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Lesser General Public
+   License as published by the Free Software Foundation; either
+   version 3 of the License, or (at your option) any later version.
+
+   This library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Lesser General Public License for more details.
+
+   You should have received a copy of the GNU Lesser General Public
+   License along with this library; if not, see <http://www.gnu.org/licenses/>.
+*/
+
+#ifndef _LDB_MDB_UTIL_H_
+#define _LDB_MDB_UTIL_H_
+
+#include <lmdb.h>
+
+#include "ldb_private.h"
+/* We need the discard_const() macros */
+#include "replace.h"
+
+/* Fills structure key with folded data from ldb_dn. The data is owned by
+ * mem_ctx. Use ldb_mdb_key_free() to free the resources.
+ */
+int ldb_mdb_dn_to_key(TALLOC_CTX *mem_ctx,
+		      struct ldb_dn *dn,
+		      struct MDB_val *key);
+
+void ldb_mdb_key_free(MDB_val *key);
+
+/* Fills structure value with data from ldb_message. The data is owned by
+ * mem_ctx. Use ldb_mdb_value_free() to free the resources.
+ */
+int ldb_mdb_msg_to_value(TALLOC_CTX *mem_ctx,
+			 struct ldb_context *ldb,
+			 struct ldb_message *msg,
+			 MDB_val *value);
+
+void ldb_mdb_value_free(MDB_val *value);
+
+#endif /* _LDB_MDB_UTIL_H_ */
